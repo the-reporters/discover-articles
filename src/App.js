9cbcb214dd5d2a90349526a07f766a2157b92ad7
Sascha Rissling <53901545+sascharissling@users.noreply.github.com>
2019-11-29 11:27:25 +0100

    add header (#3)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import GlobalStyles from "./GlobalStyles";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyles />
-      Hello
+      <Header />
     </div>
   );
 }
