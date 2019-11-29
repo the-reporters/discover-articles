d8bb6fc8bfa23a0118d3d3428cee66d8c6493c72
Sascha Rissling <53901545+sascharissling@users.noreply.github.com>
2019-11-29 11:43:01 +0100

    Add & Design footer (#4)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import GlobalStyles from "./GlobalStyles";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyles />
       <Header />
+      <Footer />
     </div>
   );
 }
